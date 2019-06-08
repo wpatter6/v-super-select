f1972574a4fc70d9c1c91f2c193cdc28ec46405e
Will Patterson <wpatter6@gmail.com>
2019-06-07 19:37:15 -0600

    Update jest config

---
+++ jest.config.js
@@ -0,0 +1,47 @@
+const testMatch = ['**/tests/**/*.spec.ts'],
+  coverageReporters = ['cobertura', 'html', 'text'];
+
+//This is an absolute hack which allows full debugging of tests, but causes code coverage to break.
+if (process.env.DEBUG_TESTS === 'true') {
+  testMatch.push('!');
+  coverageReporters.length = 0;
+}
+
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,vue}', '!**/node_modules/**', '!**/src/*'],
+  coverageReporters,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './coverage',
+        outputName: 'TEST-RESULTS.xml',
+      },
+    ],
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch,
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+};
