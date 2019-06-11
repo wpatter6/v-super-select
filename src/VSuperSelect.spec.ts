cf8e1baca4b5e1ea03f4416f74bf598c18452ad7
Will Patterson <wpatter6@gmail.com>
2019-06-11 13:08:25 -0600

    Updating tests

--- src/VSuperSelect.spec.ts
+++ src/VSuperSelect.spec.ts
@@ -1,23 +1,39 @@
 import { shallowMount } from '@vue/test-utils'
 import VSuperSelect from '@/VSuperSelect.vue'
 
+interface SelectItem {
+  text: string
+  value: string
+}
+
 describe('VSuperSelect.vue', () => {
   it('will match snapshot', () => {
     const wrapper = shallowMount(VSuperSelect, {
       propsData: {
-        items: [
-          {
-            text: 'Item 1',
-            value: '1',
-          },
-          {
-            text: 'Item 2',
-            value: '2',
-          },
-        ],
+        items: createItems(),
       },
     })
 
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('will match snapshot 2', () => {
+    const wrapper = shallowMount(VSuperSelect, {
+      propsData: {
+        items: createItems(20),
+      },
+    })
+
+    expect(wrapper).toMatchSnapshot('2')
+  })
 })
+
+const createItems = (count = 3): SelectItem[] => {
+  return Array(count)
+    .join()
+    .split(',')
+    .map((u, i) => ({
+      text: `Item ${i}`,
+      value: btoa(i + ''),
+    }))
+}
